d2814d16a3e6eb49e23b153be8c6ed5e2283e7e9
R1007 <dheerajkoila97@gmail.com>
2026-07-25 23:01:56 +0530

    DAY 7

--- list/list7.py
+++ list/list7.py
@@ -1,15 +1,8 @@
-n=int(input())
-numbers=[]
+import re
+n = int(input())
 for i in range(n):
-    number=int(input())
-    numbers.append(number)
-for number in numbers:
-    first_digit=int(str(number)[0])
-    if first_digit==7:
-        print("YES")
-    elif first_digit==8:
-        print("YES")
-    elif first_digit==9:
+    number = input()
+    if re.fullmatch(r"[789]\d{9}", number):
         print("YES")
     else:
-        print("NO")        
+        print("NO")

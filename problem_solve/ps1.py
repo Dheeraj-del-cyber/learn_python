e59213adaee9688535df8e9341a2e09f8d631297
R1007 <dheerajkoila97@gmail.com>
2026-07-31 23:30:16 +0530

    day 12

--- problem_solve/ps1.py
+++ problem_solve/ps1.py
@@ -1,9 +1,10 @@
 nums=[]
-for i in range(5):
+n=int(input("enter the number of elements:"))
+for i in range(n):
     num = int(input(f"Enter a number {i+1}:"))
     nums.append(num)
 count = 0
 for num in nums:
     if num % 2 == 0:
         count += 1
-print("The number of even numbers is:", count)            
+print("The number of even numbers is:", count) 
